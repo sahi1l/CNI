39168cdf63acbe15b5390fea1ed919987d5e3365
Sam Hill <physhill@gmail.com>
2023-08-01 23:40:08 -0400

    Update cni.py
    Fixed reference in paper comments

--- cni.py
+++ cni.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """The following code implements the Cooke-Nieboer index, as shown in the paper by Sam A. Hill found in 
-"Measure for characterizing heavy-tailed networks" in Phys. Rev. Research 3, 023257.
+"Measure for characterizing heavy-tailed networks" in Phys. Rev. Research 3, 023257 (2021).
 The class Stats implements Welford's online algorithm: 
 "Note on a method for calculating corrected sums of squares and products". Technometrics. 4 (3): 419–420."""
 import math
